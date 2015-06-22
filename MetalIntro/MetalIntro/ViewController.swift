a6d5f4cfdb7c362c2a1d6ca520d28a9dbf598ebc
David Pol <davidpol@users.noreply.github.com>
2015-06-22 15:15:27 +0200

    Add MetalIntro sample project.

---
+++ MetalIntro/MetalIntro/ViewController.swift
@@ -0,0 +1,54 @@
+import Metal
+import UIKit
+
+class ViewController: UIViewController {
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
+
+    var metalLayer: CAMetalLayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        device = MTLCreateSystemDefaultDevice()
+
+        commandQueue = device.newCommandQueue()
+
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .BGRA8Unorm
+        metalLayer.frame = view.bounds
+        view.layer.addSublayer(metalLayer)
+
+        render()
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    func render() {
+        guard let drawable = metalLayer.nextDrawable() else {
+            print("Error obtaining drawable.")
+            return
+        }
+
+        let texture = drawable.texture
+
+        let cornflowerBlue = MTLClearColorMake(100/255.0, 149/255.0, 237/255.0, 1.0)
+
+        let passDescriptor = MTLRenderPassDescriptor()
+        passDescriptor.colorAttachments[0].texture = texture
+        passDescriptor.colorAttachments[0].loadAction = MTLLoadAction.Clear
+        passDescriptor.colorAttachments[0].clearColor = cornflowerBlue
+        passDescriptor.colorAttachments[0].storeAction = MTLStoreAction.Store
+
+        let commandBuffer = commandQueue.commandBuffer()
+
+        let commandEncoder = commandBuffer.renderCommandEncoderWithDescriptor(passDescriptor)
+        commandEncoder.endEncoding()
+
+        commandBuffer.presentDrawable(drawable)
+        commandBuffer.commit()
+    }
+}
